e17878d5f9975bcfaccdb6553ec9e6bf576649a0
nestharus <mrasolomon@gmail.com>
2013-12-11 19:38:46 -0800

    kk

--- src/editor/Main.java
+++ src/editor/Main.java
@@ -13,56 +13,7 @@ public class Main {
             public void run() {
                 //Turn off metal's use of bold fonts
                 UIManager.put("swing.boldMetal", Boolean.FALSE);
-                //Editor.createAndShowGUI();
-                
-                JFrame frame = new JFrame();
-                
-                frame.setTitle("Lists");
-                frame.setSize(400, 400);
-                frame.setLocationRelativeTo(null);
-                
-                JPanel panel = new JPanel();
-                
-                panel.setSize(new Dimension(400, 200));
-                
-                panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-                
-                /*
-                    Toolbar
-                */
-                Toolbar toolbar = new Toolbar();
-                
-                toolbar.add(new JLabel("test 1"));
-                toolbar.add(new JLabel("test 2"));
-                toolbar.add(new JLabel("test 3"));
-                
-                toolbar.setSize(new Dimension(200, 300));
-                
-                //toolbar.setDropMode(DropMode.INSERT);
-                
-                panel.add(toolbar);
-                
-                /*
-                    Canvas
-                */
-                Canvas canvas = new Canvas();
-                DefaultListModel<Object> canvasList = (DefaultListModel<Object>)canvas.getModel();
-                
-                canvasList.addElement("test 1");
-                canvasList.addElement("test 2");
-                canvasList.addElement("test 3");
-                canvasList.addElement("test 1");
-                
-                canvas.setSize(new Dimension(200, 300));
-                
-                canvas.setDropMode(DropMode.INSERT);
-                
-                panel.add(canvas);
-                
-                frame.add(panel);
-                
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
+                Editor.createAndShowGUI();
             }
         });
     }
